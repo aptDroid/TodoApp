316fbee1bc547af646ee17d1194b11ed55bf0313
aptDroid <apt.sharp@gmail.com>
2017-10-19 13:31:56 +0530

    Update actions.js

--- actions/actions.js
+++ actions/actions.js
@@ -2,15 +2,3 @@
 export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const DELETE_TODO = "DELETE_TODO";
-
-export function addTodo(text) {
-  return { type: ADD_TODO, text }
-}
-
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index }
-}
-
-export function deleteTodo(index) {
-  return { type: DELETE_TODO, index }
-}
